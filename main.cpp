7e6bfb5f57f2fe269657387cf5b2c942b05c0114
Aref Malek <arefmalek02@gmail.com>
2021-07-29 19:35:24 -0700

    I'm going to need to pick up on what's going on with the vec class again, otherwise this has been a smooth operation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-#include "vec3.h"
+#include "ray.h"
+
+vec3 color(const ray& r) 
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+}
 
 int main()
 {
@@ -8,15 +15,26 @@ int main()
     int ny = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+
+
     for (int j = ny -1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            vec3 col(
-                    float(i) / float(nx),
-                    float(j) / float(ny),
-                    0.2
-                    );
+            // as expected, u and v are the unit vectors here 
+            float u = float(i) / float(nx); // x unit vector
+            float v = float(j) / float(ny); // y unit vector
+
+            if (u > 1.0 || v > 1.0) std::cout << "danger!!\n";
+
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = color(r);
+
+
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
@@ -24,6 +42,5 @@ int main()
             std::cout << ir << " " << ig << " " << ib << std::endl;
         }
     }
-
     return 0;
 }
